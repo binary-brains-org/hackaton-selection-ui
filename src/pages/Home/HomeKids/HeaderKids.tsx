5259dcf37f01fa09978a1dcee545f05275548f1a
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-26 23:31:02 +0300

    feat: add header for kids

--- src/pages/Home/HomeKids/HeaderKids.tsx
+++ src/pages/Home/HomeKids/HeaderKids.tsx
@@ -1,25 +1,25 @@
-import { IoWallet } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { FaMoneyBills } from "react-icons/fa6";
 
 const HeaderKids = () => {
   return (
     <header className="p-2 w-full flex items-center justify-between shadow">
-      <Link
-        to="/"
-        className="flex gap-2 items-center text-blue-600"
-      >
-        <IoWallet className="text-3xl" />
-        <span className="text-xl font-bold">VisualPayKids</span>
-      </Link>
+      <div className="h-full text-[1.1rem] flex items-center">
+        <span className="text-violet-500 font-semibold flex items-center gap-2">
+          <FaMoneyBills className="text-[1.5rem]" />
+          <span>Your balance:&nbsp;</span>
+        </span>
+        <span className="text-gray-800">{400}Ar</span>
+      </div>
 
       <Link
         to="/"
-        className="w-[2.5rem] h-[2.5rem] sticky text-white bg-gray-500 rounded-full center-flex text-lg font-bold"
+        className="w-[2.5rem] h-[2.5rem] bg-gray-500 rounded-full center-flex font-bold"
       >
         A
       </Link>
     </header>
-  )
-}
+  );
+};
 
 export default HeaderKids;
