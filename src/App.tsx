e2b71b864f03399e36b8b63fb9176a8afa9e0a58
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-01-26 18:20:40 +0100

    feat: Routes for the applications path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 function App() {
-  return <></>;
+ 
+  return (
+    <Router>
+      <Routes>
+        <Route element={<>hello world</>} path="/test"/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
