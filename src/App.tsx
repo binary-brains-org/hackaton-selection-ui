73ce0388afa01ecefbb7c194f03c8ac02a08362b
ToavinaMaria <ToavinaMaria>
2024-01-26 23:08:53 +0300

    style: reformat SignUpParent

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import SignUpParent from "./pages/SignUpParent";
+
 function App() {
-  return <></>;
+  return <SignUpParent item={{age: 1, firstname: "Maria",
+    lastname: "Rnd",
+    birthdate: "2006-01-01",
+    role: "Child",
+    sex: "F",
+    age_category: "Kid",
+    password: "Maria",
+    cin: "0005 0938 0099FTD"}}/>;
 }
 
 export default App;
