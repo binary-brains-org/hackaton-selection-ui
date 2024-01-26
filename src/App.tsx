2a9a600ca0b08e56cc4a13feb80e2f24d74de46f
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-01-26 18:55:17 +0100

    feat: created Login file and route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/Login/Login";
 
 function App() {
  
@@ -6,6 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route element={<>hello world</>} path="/test"/>
+        <Route Component={LoginPage} path="/Login"/>
       </Routes>
     </Router>
   );
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function LoginPage() : React.ReactElement{
+  return(
+    <>login</>
+  )
+}
