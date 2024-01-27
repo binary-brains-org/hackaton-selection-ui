b12d340fc458d5f9a1b51997c42b9cf6d62057ed
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-27 13:34:54 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
+import HomeParent from "./pages/Home/HomeParent";
 import { createTheme, ThemeProvider } from "@mui/material";
+import Login from "./pages/Login";
 
 const theme = createTheme({
   palette: {
     primary: {
       main: "#FF8400",
-      contrastText: "#000"
-    }
-  }
+      contrastText: "#000",
+    },
+  },
 });
 
 const App = () => {
@@ -18,13 +20,13 @@ const App = () => {
         <Routes>
           <Route Component={Home} path="/" />
           <Route Component={Home} path="/home/kid" />
-          <Route Component={Home} path="/home/parent" />
+          <Route Component={HomeParent} path="/home/parent" />
 
           <Route Component={null} path="/view/parent/:user_id" />
           <Route Component={null} path="/view/kid/:user_id" />
 
           <Route Component={null} path="/signup" />
-          <Route Component={null} path="/login" />
+          <Route Component={Login} path="/login" />
         </Routes>
       </Router>
     </ThemeProvider>
