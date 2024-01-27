3a7791da5a759056059a8e919dc75ad3d051100d
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-27 10:55:27 +0300

    feat: add image

---
+++ src/pages/Home/HomeParent/Nav.tsx
@@ -0,0 +1,40 @@
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import {
+  Link,
+  matchPath,
+  useLocation,
+} from 'react-router-dom';
+
+function useRouteMatch(patterns: readonly string[]) {
+  const { pathname } = useLocation();
+
+  for (let i = 0; i < patterns.length; i += 1) {
+    const pattern = patterns[i];
+    const possibleMatch = matchPath(pattern, pathname);
+    if (possibleMatch !== null) {
+      return possibleMatch;
+    }
+  }
+
+  return null;
+}
+
+function MyTabs() {
+  const routeMatch = useRouteMatch(['/home/parent', '/drafts', '/trash']);
+  const currentTab = routeMatch?.pattern?.path;
+  return (
+    <Tabs value={currentTab}>
+      <Tab label="My children" value="/home/parent" to="/home/parent" component={Link} />
+      <Tab label="Deposite" value="/drafts" to="/drafts" component={Link} />
+      <Tab label="Transfer" value="/trash" to="/trash" component={Link} />
+    </Tabs>
+  );
+}
+
+const Nav = () => {
+  return <MyTabs />
+}
+
+
+export default Nav;
---
+++ src/pages/Home/HomeParent/index.tsx
@@ -0,0 +1,17 @@
+import HeaderBase from "../../../components/HeaderBase.tsx";
+import Nav from "./Nav.tsx";
+
+const HomeParent = () => {
+  return (
+    <div>
+      <HeaderBase>
+        <Nav />
+      </HeaderBase>
+
+      <div className="w-full flex">
+      </div>
+    </div>
+  )
+}
+
+export default HomeParent;
