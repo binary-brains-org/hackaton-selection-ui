8800fd1e326279302a5d2fc6be47c938acf4af87
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-01-27 01:30:59 +0100

    feat: creating signUp page

---
+++ src/pages/SignUp/Stepper.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+const steps = [
+  'Basic Information',
+  'User',
+  'rest',
+];
+
+export default function HorizontalLinearAlternativeLabelStepper(props:{activeStep:number}) {
+  const { activeStep } = props;
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}
