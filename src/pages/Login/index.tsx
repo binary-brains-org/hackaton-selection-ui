5168a07a03342b16598d65e06c643f65c81c7385
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-27 16:40:51 +0300

    feat: login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,31 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import AppLogo from "../../components/AppLogo.tsx";
+import { useForm } from "react-hook-form";
+import { LoginUser, loginUser } from "../../api";
+import { useState } from "react";
+import DialogError from "../../components/DialogError.tsx";
+import { useNavigate } from "react-router-dom";
+import local from "../../core/local.ts";
 
 const Login = () => {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const {
+    register,
+    handleSubmit,
+  } = useForm<LoginUser>();
+  const nav = useNavigate();
+  const [error, setError] = useState<Error | null>(null);
 
   return (
-    <Grid container component="main" sx={{ height: '100vh' }}>
+    <Grid container component="main" sx={{ height: "100vh" }}>
       <CssBaseline />
       <Grid
         item
@@ -30,12 +33,14 @@ const Login = () => {
         sm={4}
         md={7}
         sx={{
-          backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-          backgroundRepeat: 'no-repeat',
+          backgroundImage: "url(/kids_financing.jpg)",
+          backgroundRepeat: "no-repeat",
           backgroundColor: (t) =>
-            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
       />
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -43,37 +48,58 @@ const Login = () => {
           sx={{
             my: 8,
             mx: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            P
-          </Avatar>
+          <AppLogo />
           <Typography component="h1" variant="h5">
-            Sign in
+            Welcome back !
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit(v => {
+              loginUser(v)
+                .then((d) => {
+                  local.setUser(d);
+                  nav("/");
+                })
+                .catch(setError);
+            })}
+            sx={{ mt: 1 }}
+          >
             <TextField
+              {...register("firstname")}
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
+              id="firstname"
+              label="firstname"
+              name="firstname"
+              autoComplete="firstname"
               autoFocus
             />
             <TextField
+              {...register("lastname")}
+              margin="normal"
+              required
+              fullWidth
+              name="lastname"
+              label="lastname"
+              type="lastname"
+              id="lastname"
+            />
+            <TextField
+              {...register("password")}
               margin="normal"
               required
               fullWidth
               name="password"
-              label="Password"
+              label="password"
               type="password"
               id="password"
-              autoComplete="current-password"
             />
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
@@ -102,8 +128,10 @@ const Login = () => {
           </Box>
         </Box>
       </Grid>
+
+      {error ? DialogError(error) : null}
     </Grid>
   );
-}
+};
 
 export default Login;
