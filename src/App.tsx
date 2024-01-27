a9e46747c1e69022b8e4ce7d286709745ea952f5
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-27 09:58:27 +0300

    feat: theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,33 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home.tsx";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#FF8400",
+      contrastText: "#000"
+    }
+  }
+});
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route Component={Home} path="/" />
-        <Route Component={Home} path="/home/kid" />
-        <Route Component={Home} path="/home/parent" />
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route Component={Home} path="/" />
+          <Route Component={Home} path="/home/kid" />
+          <Route Component={Home} path="/home/parent" />
 
-        <Route Component={null} path="/view/parent/:user_id" />
-        <Route Component={null} path="/view/kid/:user_id" />
+          <Route Component={null} path="/view/parent/:user_id" />
+          <Route Component={null} path="/view/kid/:user_id" />
 
-        <Route Component={null} path="/signup" />
-        <Route Component={null} path="/login" />
-      </Routes>
-    </Router>
+          <Route Component={null} path="/signup" />
+          <Route Component={null} path="/login" />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 };
 
