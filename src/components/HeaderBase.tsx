dd4210aee006604449db89fcc745017661d0516a
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-27 13:35:54 +0300

    feat: add user infos

--- src/components/HeaderBase.tsx
+++ src/components/HeaderBase.tsx
@@ -3,7 +3,7 @@ import { Avatar, Box, styled } from "@mui/material";
 import { MdAttachMoney } from "react-icons/md";
 import AppLogo from "./AppLogo.tsx";
 
-const HeaderBase = ({children}: PropsWithChildren) => {
+const HeaderBase = ({ children }: PropsWithChildren) => {
   return (
     <Header>
       <AppLogo />
@@ -18,20 +18,26 @@ const HeaderBase = ({children}: PropsWithChildren) => {
 
         <button className="flex gap-1.5">
           <div className="flex flex-col">
-            <span className="text-lg leading-tight font-semibold">Firstname</span>
-            <span className="text-sm leading-tight text-end text-gray-500">Lastname</span>
+            <span className="text-lg leading-tight font-semibold">
+              Firstname
+            </span>
+            <span className="text-sm leading-tight text-end text-gray-500">
+              Lastname
+            </span>
           </div>
-          <Avatar alt="profile here" src="">P</Avatar>
+          <Avatar alt="profile here" src="">
+            P
+          </Avatar>
         </button>
       </Box>
     </Header>
-  )
-}
+  );
+};
 
 const Header = styled("header")({
   width: "100%",
   display: "flex",
   padding: ".8rem",
-  justifyContent: "space-between"
+  justifyContent: "space-between",
 });
 export default HeaderBase;
