64a8a71df37c5e1201a19a1c421c5928c71222ce
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2024-01-27 09:21:56 +0300

    feat: add app logo

---
+++ src/components/AppLogo.tsx
@@ -0,0 +1,16 @@
+import { FaGoogleWallet } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const AppLogo = () => {
+  return (
+    <Link
+      to="/"
+      className="text-[--color-orange] flex gap-2 items-center font-semibold"
+    >
+      <FaGoogleWallet className="text-2xl" />
+      <span className="text-xl">VisualPayKids</span>
+    </Link>
+  )
+}
+
+export default AppLogo;
